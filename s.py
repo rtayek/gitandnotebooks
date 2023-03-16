986d25a7902925179bd66b51bb8495098be5632c
rtayek <rtayek@gmail.com>
2023-03-15 20:16:06 -0700

    made class for series.

---
+++ s.py
@@ -0,0 +1,12 @@
+class S(): # class for series
+    def __init__(self):
+        pass
+    def __str__(self):
+        return "class for series"
+        TIME, SERIES = generate_time_series()
+    splitTie = 1100
+    windoeSize = 20
+    batchSize = 32
+    shuffkeBufferSize = 1000
+s=S()
+print(s)    
